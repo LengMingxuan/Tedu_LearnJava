f8a535d646d3fd22bd7353520a3f9dc9cda3a752
LengMingxuan <983835745@qq.com>
2020-07-26 14:30:49 +0800

    修改内容

--- src/Main/MainTest.java
+++ src/Main/MainTest.java
@@ -3,6 +3,7 @@ package Main;//包名
 public class MainTest /*类名和文件名想通*/{
     //1个文件里只有一个类，其他的类叫内部类
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.print("Hello World!\n");//不能自动添加换行符
+        System.out.println("Hello World!");//自动添加换行符
     }
 }
